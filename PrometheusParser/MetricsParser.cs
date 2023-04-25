e5f37d3cfd161ef3f37c090f5b8289954cb1b6a3
chintu10cs25 <chintu10cs25@gmail.com>
2023-04-25 18:39:19 +0530

    Parsing refactored

--- PrometheusParser/MetricsParser.cs
+++ PrometheusParser/MetricsParser.cs
@@ -49,7 +49,82 @@ namespace PrometheusParser
                     _metrics.Add(name, new Metric(name, dataPoint));
             }
         }
+        public void Load(string PromResponseFilePath)
+        {
+            List<string[]> rawMetrics = GetRawMetrics(PromResponseFilePath);
+            foreach (string[] metrics in rawMetrics)
+            {
+                Parse(metrics);
+            }
+        }
+        private void Parse(string[] rawMetricLines)
+        {
+            string helpLine = rawMetricLines[0];
+            string typeLine = rawMetricLines[1];
+            //split typeLine to extract name and type
+            string[] parts = typeLine.Split(' ');
+            string name = parts[2];
+            string type = parts[3];
+            if (type == "counter" || type == "gauge")
+            {
+                for (int i = 2; i < rawMetricLines.Length; i++)
+                {
+                    string metricLine = rawMetricLines[i];
+                    DataPoint dataPoint = GetDataPoint(metricLine);
+                    // Add the metric to the dictionary               
+                    if (_metrics.ContainsKey(name))
+                        _metrics[name].AddDataPoint(dataPoint);
+                    else
+                        _metrics.Add(name, new Metric(name, dataPoint));
+                }
+            }
+        }
+        public List<string[]> GetRawMetrics(string PromResponseFilePath)
+        {
+            List<string[]> rawMetrics = new List<string[]>();
+            // Read the file and split the response into lines         
+            string[] lines = File.ReadAllLines(PromResponseFilePath).Skip(1).ToArray();
 
+            // Loop through the lines and parse the metrics
+            int commentCounter = 0;
+            List<string> rawMetricLines = new List<string>();
+            
+            foreach (string line in lines)
+            {
+                // Skip the empty lines
+                if (string.IsNullOrWhiteSpace(line))
+                    continue;
+
+                if (line.StartsWith("#"))
+                {
+                    ++commentCounter;
+                    if (commentCounter == 1)
+                    {
+                        rawMetricLines = new List<string>();
+                        rawMetricLines.Add(line);
+                    }
+                    else if(commentCounter > 1 && commentCounter < 3)
+                    {
+                        rawMetricLines.Add(line);
+                    }
+                    else if (commentCounter >= 3)
+                    {
+                        string[] temp = rawMetricLines.ToArray<string>();
+                        rawMetrics.Add(temp);
+                        rawMetricLines = new List<string>();
+                        rawMetricLines.Add(line);
+                        commentCounter = 1;
+                    }
+                }
+                else
+                {
+                    rawMetricLines.Add(line);
+                }
+
+            }
+
+            return rawMetrics;
+        }
         public IDictionary<string,Metric> GetMetrics()
         {
             return _metrics;
